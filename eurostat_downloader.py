6a97b936d1051a78755360ddc5f41662c6477cf8
Cuvuliuc Alex-Andrei <cuvuliucalexandrei@gmail.com>
2024-01-03 20:23:58 +0200

    added ui

--- eurostat_downloader.py
+++ eurostat_downloader.py
@@ -28,7 +28,7 @@ from qgis.PyQt.QtWidgets import QAction
 # Initialize Qt resources from file resources.py
 from .resources import *
 # Import the code for the dialog
-from .src.src.eurostat_downloader_dialog import EurostatDownloaderDialog
+from .src.eurostat_downloader_dialog import EurostatDownloaderDialog
 import os.path
 
 
