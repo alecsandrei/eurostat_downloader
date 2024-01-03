c52da950ee3b633a789933e8975a6eaddef05c7b
Cuvuliuc Alex-Andrei <cuvuliucalexandrei@gmail.com>
2024-01-03 20:20:40 +0200

    added init

--- test/test_eurostat_downloader_dialog.py
+++ test/test_eurostat_downloader_dialog.py
@@ -16,7 +16,7 @@ import unittest
 
 from qgis.PyQt.QtGui import QDialogButtonBox, QDialog
 
-from eurostat_downloader_dialog import EurostatDownloaderDialog
+from src.eurostat_downloader_dialog import EurostatDownloaderDialog
 
 from utilities import get_qgis_app
 QGIS_APP = get_qgis_app()
