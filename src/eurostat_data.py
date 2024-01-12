716e3de38742c77f64d2156257b3f27ea3575bf9
Cuvuliuc Alex Andrei <cuvuliucalexandrei@gmail.com>
2024-01-12 19:31:03 +0200

    added more objects

--- src/eurostat_data.py
+++ src/eurostat_data.py
@@ -1,22 +1,20 @@
+from typing import Union
 from functools import (
     cached_property,
     lru_cache
 )
 from difflib import SequenceMatcher
-
-import eurostat
-import pandas as pd
-from pandas import DataFrame
 from dataclasses import (
     dataclass,
     field
 )
 
-from .utils import PyQtUtils
+import eurostat
+import pandas as pd
 
 
 
-@dataclass(slots=False)
+@dataclass
 class EstatDatabase:
 
     @cached_property
@@ -40,21 +38,30 @@ class EstatDatabase:
         """Computes string similarity between two strings."""
         return SequenceMatcher(None, a, b).ratio()
 
-    @lru_cache(maxsize=16)
     def get_subset(self, keyword: str):
         """Creates a subset of the toc. Uses SequenceMatcher to sort the results."""
+        if not keyword:
+            return self.toc
         keyword = keyword.lower()
         self.toc['match_ratio_title'] = self.toc['title'].apply(lambda x: self.compute_string_similarity(keyword, x.lower()))
         self.toc['match_ratio_code'] = self.toc['code'].apply(lambda x: self.compute_string_similarity(keyword, x.lower()))
         self.toc['match_ratio'] = self.toc['match_ratio_title'] + self.toc['match_ratio_code']
-        subset = (self.toc.loc[self.toc['match_ratio'] > 0.5]
-                          .sort_values(by='match_ratio', ascending=False))
-        self.toc.drop(columns=['match_ratio_title','match_ratio_code', 'match_ratio'], inplace=True)
-        subset.drop_duplicates(inplace=True)
+        subset = self.toc.loc[self.toc['match_ratio'] > 0.5].sort_values(by='match_ratio', ascending=False)
+        self.toc.drop(columns=['match_ratio_title', 'match_ratio_code', 'match_ratio'], inplace=True)
         return subset
 
+    def get_titles(self, subset: Union[None, pd.DataFrame] = None):
+        if subset is None:
+            subset = self.toc
+        return subset['title']
+    
+    def get_codes(self, subset: Union[None, pd.DataFrame, pd.Series] = None):
+        if subset is None:
+            subset = self.toc
+        return subset['code']
 
-@dataclass(slots=False)
+
+@dataclass
 class EstatDataset(EstatDatabase):
     """
     TODO: fix this
@@ -68,38 +75,22 @@ class EstatDataset(EstatDatabase):
     def title(self):
         return self.toc.loc[self.toc['code'] == self.code, 'title']
 
-    @cached_property
-    def data_start(self):
-        return self.date_columns[0]
+    # @cached_property
+    # def data_start(self):
+    #     return self.date_columns[0]
 
-    @cached_property
-    def data_end(self):
-        return self.date_columns[-1]
+    # @cached_property
+    # def data_end(self):
+    #     return self.date_columns[-1]
     
-    @cached_property
-    def date_columns(self):
-        return self.data.columns[len(self.params):]
+    # @cached_property
+    # def date_columns(self):
+    #     return self.data.columns[len(self.params):]
 
     @cached_property
-    def data(self):
-        data = eurostat.get_data_df(code=self.code)
-        assert data is not None, 'data is none'
-        time_period_column = [column for column in data.columns if column.endswith('TIME_PERIOD')]
-        if len(time_period_column) == 1:
-            time_period_column = time_period_column[0]
-            data = data.rename(columns={time_period_column: time_period_column.split('\\')[0]})
-        return data
+    def df(self) -> pd.DataFrame:
+        return eurostat.get_data_df(code=self.code)
     
     @cached_property
     def params(self):
         return eurostat.get_pars(self.code)
-
-
-@dataclass
-class EstatDatasetModel:
-    estat_dataset: EstatDataset
-    df: DataFrame
-    
-    @cached_property
-    def model(self) -> PyQtUtils.PandasModel:
-        return PyQtUtils.PandasModel(data=self.df)
--- src/eurostat_downloader.py
+++ src/eurostat_downloader.py
@@ -1,25 +1,114 @@
+from __future__ import annotations
 
-from qgis.PyQt.QtWidgets import (
-    QDialog
+from dataclasses import (
+    dataclass,
+    field
+)
+from functools import cached_property
+
+import pandas as pd
+from qgis.PyQt import (
+    QtCore,
+    QtWidgets
 )
 
-from .ui import EurostatDialogBase
+from .ui import (
+    UIEurostatDialog,
+    UIEurostatParameterSectionDialog
+)
 from .eurostat_data import (
     EstatDatabase,
     EstatDataset
 )
 
-class EurostatDialog(QDialog):
+
+@dataclass(slots=False, init=False)
+class EurostatDialog(QtWidgets.QDialog):
+    ui: UIEurostatDialog = field(init=False)
+    database: EstatDatabase = field(init=False)
+    dataset: EstatDataset = field(init=False)
+    subset: pd.DataFrame = field(init=False)
+    model: EstatDatasetModel = field(init=False)
+    
     def __init__(self):
         super().__init__()
-        ui = EurostatDialogBase()
-        ui.setupUi(self)
-        self.ui = ui
+        self.ui = UIEurostatDialog()
+        self.ui.setupUi(self)
         self.database = EstatDatabase()
-        
+
         # Signals
-        self.ui.pbSearch.clicked.connect(self.populate_list)
+        self.ui.buttonSearch.clicked.connect(self.populate_list)
+        self.ui.listDatabase.itemSelectionChanged.connect(self.dataset_to_table)
+        self.ui.tableDataset.horizontalHeader().sectionDoubleClicked.connect(self.open_section_ui)
+        
+    def populate_list(self):
+        self.subset = self.database.get_subset(keyword=self.ui.lineSearch.text())
+        titles = self.database.get_titles(subset=self.subset)
+        codes = self.database.get_codes(subset=self.subset)
+        self.ui.listDatabase.addItems('[' + codes + '] ' + titles)
+
+    def get_selected_dataset_code(self):
+        row = self.ui.listDatabase.currentRow()
+        return self.database.get_codes(subset=self.subset).iloc[row]
+
+    def dataset_to_table(self):
+        self.dataset = EstatDataset(database=self.database, code=self.get_selected_dataset_code())
+        self.model = EstatDatasetModel(estat_dataset=self.dataset, df=self.dataset.df)
+        self.ui.tableDataset.setModel(self.model.pandas)
+
+    def get_selected_section_name(self):
+        clicked_section_index = self.ui.tableDataset.horizontalHeader().currentIndex().column()
+        return self.model.df.columns[clicked_section_index]
+
+    def open_section_ui(self):
+        EurostatParameterSectionDialog(name=self.get_selected_section_name(), base=self)
+
+
+@dataclass(init=False)
+class EurostatParameterSectionDialog(QtWidgets.QDialog):
+    
+    def __init__(self, name: str, base: EurostatDialog):
+        super().__init__()
+        self.name = name
+        self.base = base
+        self.ui = UIEurostatParameterSectionDialog()
+        self.ui.setupUi(self)
+        self.populate_list()
+        self.exec_()
 
     def populate_list(self):
-        keyword = self.ui.searchDatabase.text()
-        subset = self.database.get_subset(keyword)
+        self.ui.listItems.addItems(self.base.model.df[self.name].unique())
+
+
+@dataclass
+class EstatDatasetModel:
+    estat_dataset: EstatDataset
+    df: pd.DataFrame
+    
+    @cached_property
+    def pandas(self) -> PandasModel:
+        return PandasModel(data=self.df)
+
+
+class PandasModel(QtCore.QAbstractTableModel):
+    """Class to turn a pandas dataframe into a QAbstractTableModel."""
+    def __init__(self, data: pd.DataFrame, parent=None):
+        QtCore.QAbstractTableModel.__init__(self, parent)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parent=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=QtCore.Qt.ItemDataRole.DisplayRole):
+        if index.isValid():
+            if role == QtCore.Qt.ItemDataRole.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == QtCore.Qt.Orientation.Horizontal and role == QtCore.Qt.ItemDataRole.DisplayRole:
+            return self._data.columns[col]
+        return None
